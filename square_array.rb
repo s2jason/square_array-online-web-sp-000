68494b4f33f82b6944ce11dbaa0997386e5bb9cd
Jason Keeling <s2jason@yahoo.com>
2020-03-15 18:57:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,5 @@
 def square_array(array)
   array = []
-  array.each {|i| array << i**2}
+  array.each { |i| array << i ** 2 }
   array
-end
-
-
-
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
 end
